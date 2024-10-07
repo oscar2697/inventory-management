57b270669230ffebe5e10a06ff940b1661e5c983
oscar2697 <lindooscar635@gmail.com>
2024-10-07 16:41:19 -0500

    Deployment

---
+++ client/src/app/dashboard/DashboardWrapper.tsx
@@ -0,0 +1,17 @@
+import Navbar from "../(components)/Navbar/index"
+import Sidebar from "../(components)/Sidebar"
+
+const DashboardWrapper = ({children}: {children: React.ReactNode}) => {
+    return (
+        <div className={'light flex bg-gray-50 text-gray-900 w-full min-h-screen'}>
+            <Sidebar/>
+            
+            <main className={'flex flex-col w-full h-full py-7 px-8 bg-gray-50 md:pl-24'}>
+                <Navbar/>
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default DashboardWrapper
