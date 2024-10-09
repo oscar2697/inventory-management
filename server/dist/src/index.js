7497a977d38b6c7897e73e063b22ee6887ba2a1f
oscar2697 <lindooscar635@gmail.com>
2024-10-09 15:30:22 -0500

    "Removed hardcoded dashboard route and replaced with dashboardRoutes"

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -18,9 +18,7 @@ app.use((0, morgan_1.default)('common'));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
-app.use('/dashboard', (req, res) => {
-    res.send('Hello World!');
-});
+app.use('/dashboard', dashboardRoutes);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
