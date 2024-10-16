4b12eeecfefe6d50d6ab1e77458a3da645187f9f
oscar2697 <lindooscar635@gmail.com>
2024-10-15 23:35:33 -0500

    "Update ESLint rules and refactor Redux store configuration"

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -9,7 +9,6 @@ import {
 import globalReducer from "@/state";
 import { api } from "@/state/api";
 import { setupListeners } from "@reduxjs/toolkit/query";
-
 import {
     persistStore,
     persistReducer,
@@ -23,22 +22,22 @@ import {
 import { PersistGate } from "redux-persist/integration/react";
 import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 
-// Crear un almacenamiento "no operante" en caso de que el entorno no sea el navegador
 const createNoopStorage = () => {
     return {
         getItem() {
             return Promise.resolve(null);
         },
         setItem(_key: string, value: string) {
+            console.log(`Guardando en almacenamiento con clave: ${_key}`);
             return Promise.resolve(value);
         },
         removeItem(_key: string) {
+            console.log(`Eliminando del almacenamiento con clave: ${_key}`);
             return Promise.resolve();
         },
     };
 };
 
-// Determinar si estamos en un entorno de servidor o navegador
 const storage =
     typeof window === "undefined"
         ? createNoopStorage()
@@ -47,19 +46,16 @@ const storage =
 const persistConfig = {
     key: "root",
     storage,
-    whitelist: ["global"], // Especifica qué reducers quieres persistir
+    whitelist: ["global"], 
 };
 
-// Combina los reducers
 const rootReducer = combineReducers({
     global: globalReducer,
     [api.reducerPath]: api.reducer,
 });
 
-// Aplica el reducer persistente
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// Configuración de la store con redux-persist y api middleware
 export const makeStore = () => {
     return configureStore({
         reducer: persistedReducer,
@@ -72,27 +68,21 @@ export const makeStore = () => {
     });
 };
 
-// Tipos personalizados para la store y el dispatch
 export type AppStore = ReturnType<typeof makeStore>;
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
 
-// Hooks personalizados para usar en los componentes
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-// Componente del proveedor de la store
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
-    // Referencia de la store
     const storeRef = useRef<AppStore | null>(null);
 
-    // Inicializar la store solo una vez
     if (!storeRef.current) {
         storeRef.current = makeStore();
         setupListeners(storeRef.current.dispatch);
     }
 
-    // Persistir la store
     const persistor = persistStore(storeRef.current);
 
     return (
